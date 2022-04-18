c9ebf938dd5f9a7d2a2572055ca21bbdb72bbc47
Andrii Leshko <a.leshko@foodtech.team>
2022-04-18 19:46:08 +0300

    init

---
+++ src/BpmWorkflowBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Gupalo\BpmWorkflowBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BpmWorkflowBundle extends Bundle
+{
+}
---
+++ src/DependencyInjection/BpmWorkflowExtension.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Gupalo\BpmWorkflowBundle\DependencyInjection;
+
+use Exception;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class BpmWorkflowExtension extends Extension
+{
+    /**
+     * @param array $configs
+     * @param ContainerBuilder $container
+     * @throws Exception
+     */
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yaml');
+    }
+}

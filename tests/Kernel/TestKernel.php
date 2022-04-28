4b3ceef97ff3453b37f5b1f5a50558c231499454
vl <vladimir.luchaninov@gmail.com>
2022-04-28 22:50:19 +0200

    made tests working

---
+++ tests/Kernel/TestKernel.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Gupalo\BpmmWorkflowBundle\Tests\Kernel;
+
+use Gupalo\BpmnWorkflowBundle\BpmnWorkflowBundle;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Kernel;
+
+class TestKernel extends Kernel
+{
+    public function registerBundles(): iterable
+    {
+        $bundles = [];
+
+        if ($this->getEnvironment() === 'test') {
+            $bundles[] = new FrameworkBundle();
+            $bundles[] = new BpmnWorkflowBundle();
+        }
+
+        return $bundles;
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__ . '/config.yaml');
+    }
+}

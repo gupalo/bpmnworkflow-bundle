39065b7299fadae4b9006b404aa47ccfa25aeaf9
Andrii Leshko <a.leshko@foodtech.team>
2022-05-12 15:12:16 +0300

    trace

--- src/Process/Workflow.php
+++ src/Process/Workflow.php
@@ -7,6 +7,7 @@ use Gupalo\BpmnWorkflow\Context\ContextInterface;
 use Gupalo\BpmnWorkflow\Extension\ExtensionHandler;
 use Gupalo\BpmnWorkflow\Process\ProcessWalker;
 use Gupalo\BpmnWorkflow\Process\Workflow as WorkflowCore;
+use Gupalo\BpmnWorkflow\Trace\TraceStorageInterface;
 use Traversable;
 
 class Workflow
@@ -19,9 +20,14 @@ class Workflow
         $this->processWalker = new ProcessWalker(new ExtensionHandler($extension));
     }
 
-    public function executeProcess(BpmnLoaderInterface $bpmnLoader, ContextInterface $context, string $firstProcessName): void
-    {
-        $workflow = new WorkflowCore($bpmnLoader, $this->processWalker);
+    public function executeProcess(
+        BpmnLoaderInterface $bpmnLoader,
+        ContextInterface $context,
+        string $firstProcessName,
+        bool $saveTrace = false,
+        ?TraceStorageInterface $traceStorage = null
+    ): void {
+        $workflow = new WorkflowCore($bpmnLoader, $this->processWalker, $saveTrace, $traceStorage);
         $workflow->walk($firstProcessName, $context);
     }
 }
